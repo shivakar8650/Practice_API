6144f988023a427cc5c7a0acc8b9dd488e2175b7
shivakar <shivakar@ch.iitr.ac.in>
2021-12-24 23:26:28 +0530

    [add]:create the 3 tire structure

---
+++ Businesslayer/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Businesslayer
+{
+    public class Class1
+    {
+    }
+}
---
+++ Practice_API/Controllers/EmployController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Practice_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployController : ControllerBase
+    {
+        [HttpGet]
+        [Route("name")]
+        public IActionResult GetEmplyeeData()
+        {
+            List<string> empployeeData = new List<string>()
+            {
+                "shiva","naresh","vishal","ankit"
+            };
+
+            return this.Ok(new { success = true, message = "get data successfull", Data = empployeeData });
+        }
+
+        [HttpGet("Address")]
+        public IActionResult GetEmpAddress()
+        {
+            List<string> employeeData = GetAddress();
+            return this.Ok(new { success = true, message = "get data successfull", Data = employeeData });
+        }
+
+        private static List<string> GetAddress()
+        {
+            return new List<string>()
+            {
+                "delhi","haridwar","agra","mumbai"
+            };
+        }
+
+        [HttpPost("{address}")]
+        
+        public IActionResult takeAddress(string address)
+        {
+            List<string> employData = new List<string>();
+            employData.Add(address);
+            return this.Ok(new { success = true, message = "get data successfully", Data = employData });
+        }
+    }
+}
